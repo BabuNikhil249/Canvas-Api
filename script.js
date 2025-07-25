5341b2eee2bf70b379985e259626c10e8a318604
BabuNikhil249 <138782599+BabuNikhil249@users.noreply.github.com>
2025-07-25 17:52:49 +0530

    Add files via upload
    file added

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get canvas element and its drawing context
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// Set color and draw a rectangle
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
